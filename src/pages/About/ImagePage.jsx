2cf191d5df4a519a14df86ecad8d942b85c1f18f
ahadmiaji <ahadmiaji@gmail.com>
2023-11-07 01:46:55 +0600

    this is initial

---
+++ src/pages/About/ImagePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ImagePage() {
+  return (
+    <div>
+      <img
+        className="img-fluid ps-5"
+        src="https://avatars.githubusercontent.com/u/55798402?v=4"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default ImagePage;
